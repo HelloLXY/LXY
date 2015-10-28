8e21b0c5a1333bb3c4db5eedaa287075284732f3
lixiaoyong <2428557832@qq.com>
2015-10-28 17:19:20 +0800

    增加aqliteDemo

---
+++ sqliteDemo/sqliteDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sqliteDemo
+//
+//  Created by 李晓勇 on 15/10/7.
+//  Copyright (c) 2015年 李工. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sqliteDemo/sqliteDemo/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  sqliteDemo
+//
+//  Created by 李晓勇 on 15/10/7.
+//  Copyright (c) 2015年 李工. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <sqlite3.h>
+@interface ViewController : UIViewController
+
+{
+    sqlite3 *db;  
+}
+@end
+
